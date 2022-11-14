2901bf14d6715e6f3e9198c3f6cee1b701bf5cb0
tinhinane <tinhinane.ait-messaoud@univ-tlse3.fr>
2022-11-14 16:31:57 +0100

    creation classe ronin

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -2,6 +2,7 @@ package histoire;
 
 import personnages.Commercant;
 import personnages.Yakuza;
+import personnages.Ronin;
 
 public class HistoireTP4 {
 	public static void main(String[] args) {
@@ -13,6 +14,8 @@ public class HistoireTP4 {
 		Yakuza yaku = new Yakuza("Yaku Le Noir","whisky",30,"Warsong",0);
 		yaku.direBonjour();
 		yaku.extorquer(marco);
-		
+		Ronin roro= new Ronin("Roro","shochu",60);
+		roro.direBonjour();
+		roro.donner(marco);
 	}
 }
